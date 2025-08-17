26fa79c7c49d100f1a5d7b5f34b8aa2e6a44c9bf
Naman Jain <155368508+engnaman7752@users.noreply.github.com>
2025-08-17 23:07:53 +0530

    Time: 4 ms (26.82%), Space: 12.9 MB (26.82%) - LeetHub

---
+++ 0867-new-21-game/0867-new-21-game.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double new21Game(int n, int k, int maxPts) {
+        if(k==0)return 1;
+        vector<double>dp(n+1);
+        dp[0]=1;
+        double win=1;
+        for(int i=1;i<=n;i++){
+            dp[i]=win/maxPts;
+            if(i<k)
+            win+=dp[i];
+            if(i>=maxPts&&i-maxPts<k)
+            win-=dp[i-maxPts];
+           
+        }
+    double ans=0.0;
+    for(int i=k;i<=n;i++)ans+=dp[i];
+    return ans;
+        
+    }
+};
