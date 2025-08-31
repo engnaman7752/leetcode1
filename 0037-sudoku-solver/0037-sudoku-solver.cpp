e8aa9c38d17a8c25095f6bc7f15e69f29c5d7fea
Naman Jain <155368508+engnaman7752@users.noreply.github.com>
2025-08-31 14:05:31 +0530

    Time: 335 ms (57.35%), Space: 8.7 MB (62.78%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,37 +1,46 @@
 class Solution {
-private:
-    bool check(char ch, int row, int col, vector<vector<char>>& board) {
-        for (int i = 0; i < 9; i++) {
-            if (board[row][i] == ch)
-                return false;
-            if (board[i][col] == ch)
-                return false;
-            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == ch)
-                return false;
-        }
-        return true;
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        vector<vector<char>> nums = board;
+        solve(0, 0, nums, board);
     }
-    bool solve(vector<vector<char>>& board) {
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] == '.') {
-                    for (int ch = '1'; ch <= '9'; ch++) {
-                        if (check(ch, i, j, board)) {
-                            board[i][j] = ch;
-                            if (solve(board))
-                                return true;
-                            else
-                                board[i][j] = '.';
-                        }
-                    }
-                    return false;
-                }
-                
+
+    bool solve(int i, int j, vector<vector<char>>& nums, vector<vector<char>>& board) {
+        if(i == 9) { // solved entire board
+            board = nums;
+            return true;
+        }
+
+        if(nums[i][j] != '.') {
+            if(j < 8) return solve(i, j+1, nums, board);
+            else return solve(i+1, 0, nums, board);
+        }
+
+        for(char k='1'; k<='9'; k++) {
+            if(poss(i, j, k, nums)) {
+                nums[i][j] = k;
+                bool ok;
+                if(j < 8) ok = solve(i, j+1, nums, board);
+                else ok = solve(i+1, 0, nums, board);
+                if(ok) return true; 
+                nums[i][j] = '.';     
+            }
+        }
+        return false; 
+    }
+
+    bool poss(int i, int j, char k, vector<vector<char>>& nums) {
+        for(int l=0; l<9; l++) {
+            if(nums[l][j] == k) return false;
+            if(nums[i][l] == k) return false; 
+        }
+        int x = (i/3) * 3;
+        int y = (j/3) * 3;
+        for(int p=0; p<3; p++) {
+            for(int q=0; q<3; q++) {
+                if(nums[x+p][y+q] == k) return false;
             }
         }
         return true;
     }
-
-public:
-    void solveSudoku(vector<vector<char>>& board) { solve(board); }
 };
