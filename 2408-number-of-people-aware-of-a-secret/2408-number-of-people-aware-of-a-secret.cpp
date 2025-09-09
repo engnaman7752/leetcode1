3213229cb786a0edd56a90bedfae0eb860ab1422
Naman Jain <155368508+engnaman7752@users.noreply.github.com>
2025-09-09 18:58:02 +0530

    Time: 11 ms (37.13%), Space: 9.5 MB (53.22%) - LeetHub

--- 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.cpp
+++ 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     int peopleAwareOfSecret(int n, int delay, int forget) {
         const int mod = 1e9 + 7;
-        vector<long long> dp(n + 1);
+        vector<long long> dp(n + 1,0);
         dp[1] = 1;
-        long long share = 0;
 
-        for (int i = 2; i <= n; i++) {
-            if (i - delay >= 1) share = (share + dp[i - delay]) % mod;
-            if (i - forget >= 1) share = (share - dp[i - forget] + mod) % mod;
-            dp[i] = share;
+        for (int i = 1; i <= n; i++) {
+         for(int j=i+delay;j<=n&&j<=i+forget-1;j++)
+            dp[j] =(dp[j]+dp[i]+mod)%mod;
         }
 
         long long ans = 0;
