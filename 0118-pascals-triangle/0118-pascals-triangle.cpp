acc9c6fed634879f279bed2af16ae39ef60f566f
Naman Jain <155368508+engnaman7752@users.noreply.github.com>
2025-09-13 14:43:30 +0530

    Time: 0 ms (100%), Space: 9.7 MB (32.34%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
+        vector<vector<int>>ans;
         vector<int>pre;
         pre.push_back(1);
-        vector<vector<int>>ans;
         ans.push_back(pre);
         for(int i=2;i<=numRows;i++){
-            vector<int>share(i);
-            share[0]=pre[0];
-            share[i-1]=pre[pre.size()-1];
-            for(int j=1;j<i-1;j++){
-                share[j]=pre[j]+pre[j-1];
+            vector<int>post(i);
+            post[0]=1;
+            post[i-1]=1;
+            for(int i=1;i<pre.size();i++){
+                post[i]=pre[i]+pre[i-1];
             }
-            pre=share;
-            ans.push_back(share);
+            ans.push_back(post);
+            pre=post;
         }
         return ans;
         
