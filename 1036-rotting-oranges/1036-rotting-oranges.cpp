6ed08058bd3940e7cf742fb4e910c4427fa489a0
Naman Jain <155368508+engnaman7752@users.noreply.github.com>
2025-09-17 23:57:13 +0530

    Time: 0 ms (100%), Space: 16.8 MB (60.73%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,73 +1,40 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-       vector<vector<bool>> vis(grid.size(), vector<bool>(grid[0].size(), false));
+        vector<int>dr{0,-1,0,1};
+        vector<int>dc{1,0,-1,0};
         queue<pair<int,int>>q;
-        for(int i=0;i<grid.size();i++)
-        {
-            for(int j=0;j<grid[0].size();j++)
-            {
-                if(grid[i][j]==2)
-                {q.push({i,j});
-                vis[i][j]=true;}
+        int n=grid.size();
+        int m=grid[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){
+                    q.push({i,j});
+                }
             }
         }
         int ans=0;
-        int x=1;
-        int rotton=2;
-        while(!q.empty())
-    {bool flag=false;
-    int s=q.size();
-        for(int i=0;i<s;i++)
-    {int row=q.front().first;
-     int col=q.front().second;
-    q.pop();
-    if(row+1<vis.size()&&vis[row+1][col]==false&&grid[row+1][col]==x)
-    {
-        vis[row+1][col]=true;
-        grid[row+1][col]=rotton;
-        q.push({row+1,col});
-        flag=true;
-
-    }
-    if(row-1>=0&&vis[row-1][col]==false&&grid[row-1][col]==x)
-    {
-        vis[row-1][col]=true;
-        grid[row-1][col]=rotton;
-        q.push({row-1,col});
-        flag=true;
-        
-    }
-    if(col+1<vis[0].size()&&vis[row][col+1]==false&&grid[row][col+1]==x)
-    {
-        vis[row][col+1]=true;
-        grid[row][col+1]=rotton;
-        q.push({row,col+1});
-        flag=true;
-
-    }
-    if(col-1>=0&&vis[row][col-1]==false&&grid[row][col-1]==x)
-    {
-        vis[row][col-1]=true;
-        grid[row][col-1]=rotton;
-        q.push({row,col-1});
-        flag=true;
-        
-    }
-    }
-if(flag)
-ans++;
-
-    }
-     for(int i=0;i<grid.size();i++)
-        {
-            for(int j=0;j<grid[0].size();j++)
-            {
-                if(grid[i][j]==1)
-                {return -1;}
+        while(!q.empty()){
+            int s=q.size();
+            for(int i=0;i<s;i++)
+            {auto [r,c]=q.front();
+            q.pop();
+            for(int k=0;k<4;k++){
+               int row=dr[k]+r;
+                int col=dc[k]+c;
+                if(row>=0&&row<n&&col<m&&col>=0&&grid[row][col]==1){
+                    q.push({row,col});
+                    grid[row][col]=2;
+                }
+            }
+            }
+             ans++;
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1)return -1;
             }
         }
-return ans;
-        
+        return max(0,ans-1);
     }
 };
