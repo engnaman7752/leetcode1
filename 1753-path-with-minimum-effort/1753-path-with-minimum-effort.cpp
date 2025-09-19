779bda6d40f306de8594cd13caa0a5b41a4b38dc
Naman Jain <155368508+engnaman7752@users.noreply.github.com>
2025-09-19 15:46:39 +0530

    Time: 74 ms (30.86%), Space: 25.8 MB (30.2%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -3,31 +3,27 @@ public:
     int minimumEffortPath(vector<vector<int>>& heights) {
         int n=heights.size();
         int m=heights[0].size();
-        vector<vector<int>>diff(n,vector<int>(m,INT_MAX));
-        diff[0][0]=0;
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
-        pq.push({0,{0,0}});
-        vector<int>row{0,-1,0,1};
-        vector<int>col{-1,0,1,0};
-        while(!pq.empty())
-        {
-            int node_diff=pq.top().first;
-            int i=pq.top().second.first;
-            int j=pq.top().second.second;
-            pq.pop();
-            for(int l=0;l<4;l++)
-            {   
-                int new_row=i+row[l];
-                int new_col=j+col[l];
-                if(new_row>=0&&new_row<n&&new_col>=0&&new_col<m)
-                {int differ=abs(heights[i][j]-heights[new_row][new_col]);
-                    if(diff[new_row][new_col]>max(differ,node_diff))
-                    {diff[new_row][new_col]=max(differ,node_diff);
-                    pq.push({max(differ,node_diff),{new_row,new_col}});}
+        vector<vector<int>>dis(n,vector<int>(m,INT_MAX));
+        dis[0][0]=0;
+        priority_queue<tuple<int,int,int>, vector<tuple<int,int,int>>, greater<tuple<int,int,int>>> p;
+        p.push({0,0,0});
+        vector<int>dr{1,0,-1,0};
+        vector<int>dc{0,1,0,-1};
+        while(!p.empty()){
+            auto [dist,r,c]=p.top();
+            p.pop();
+            for(int i=0;i<4;i++){
+                int row=r+dr[i];
+                int col=c+dc[i];
+                if(row>=0&&row<n&&col>=0&&col<m){
+                    if(dis[row][col]>max(abs(heights[r][c]-heights[row][col]),dist))
+                    {
+                        dis[row][col]=max(abs(heights[r][c]-heights[row][col]),dist);
+                        p.push({dis[row][col],row,col});
+                    }
                 }
-
             }
         }
-        return diff[n-1][m-1];
+        return dis[n-1][m-1];
     }
 };
