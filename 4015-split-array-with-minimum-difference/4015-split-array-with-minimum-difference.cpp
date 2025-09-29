8374f1b87b0e93ebbee1adf3907b37f60662300d
Naman Jain <155368508+engnaman7752@users.noreply.github.com>
2025-09-30 00:48:52 +0530

    Time: 8 ms (40%), Space: 169.7 MB (0%) - LeetHub

---
+++ 4015-split-array-with-minimum-difference/4015-split-array-with-minimum-difference.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long splitArray(vector<int>& nums) {
+        long long leftSum=0;
+        long long rightSum=0;
+        int left=0;
+        int right=0;
+        int n=nums.size();
+        if(n==2)return abs(nums[0]-nums[1]);
+        while(left<n-1&&nums[left]<nums[left+1]){leftSum+=nums[left];left++;}
+        cout<<left;
+        right=left+1;
+        while(right<n-1){
+            if(nums[right]<=nums[right+1])return -1;
+            rightSum+=nums[right];
+            right++;
+        }
+        rightSum+=nums[right];
+        if(rightSum>leftSum||nums[left]==nums[left+1])leftSum+=nums[left];
+        else rightSum+=nums[left];
+        return abs(rightSum-leftSum);
+
+    }
+};
