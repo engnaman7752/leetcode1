c38257bee2bd99e5b41cdec67de453cb8340e6a3
Naman Jain <155368508+engnaman7752@users.noreply.github.com>
2025-12-24 17:07:39 +0530

    Time: 0 ms (100%), Space: 34.2 MB (5.29%) - LeetHub

---
+++ 3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minimumBoxes(vector<int>& apple, vector<int>& capacity) {
+        int sum=accumulate(apple.begin(),apple.end(),0);
+        sort(capacity.rbegin(),capacity.rend());
+        int ans=0;
+        for(int i:capacity){
+            sum-=i;
+            ans++;
+            if(sum<=0)return ans;
+        }
+        return 0;
+    }
+};
