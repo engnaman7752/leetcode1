63617435e4ab18228597558c3ead5bb3e1712f35
Naman Jain <155368508+engnaman7752@users.noreply.github.com>
2026-01-01 11:40:01 +0530

    Time: 0 ms (100%), Space: 11.5 MB (77.72%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,29 +1,17 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-    int n=digits.size();
-   bool flag=true;
-        if(digits[n-1]==9)
-        {
-            int i=n-1;
-            while(digits[i]==9&&i>=0)
-            {if(i==0)
-               {digits[i]=0;
-                    digits.push_back(1);
-                reverse (digits.begin(),digits.end());
-                flag=false;
-                break;}
-                digits[i]=0;
-                i--;
-              
-               
-            }
-
-            if(i>=0&&flag)
-           digits[i]++;
+        int n=digits.size();
+        if(digits[n-1]!=9)
+        {digits[n-1]++;
+        return digits;}
+        int i=n-1;
+        while(i>=0&&digits[i]==9){
+            digits[i]=0;
+            i--;
         }
-        else
-        digits[n-1]++;
+        if(i<0)digits.insert(digits.begin(), 1);
+        else digits[i]++;
         return digits;
     }
 };
