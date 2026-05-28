1b79545ef8930a6aeaa3f4da036587be72b6ca16
Naman Jain <155368508+engnaman7752@users.noreply.github.com>
2026-05-28 13:50:32 +0530

    Time: 0 ms (100%), Space: 8.5 MB (63.05%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int i=0;
+        int j=0;
+        int n=word1.size();
+        int m=word2.size();
+        string ans="";
+        while(i<n && j<m){
+            ans+=word1[i];
+            ans+=word2[j];
+            i++;
+            j++;
+        }
+        if(n>m){
+           while(i<n){
+            ans+=word1[i];
+            i++;
+        } 
+        }
+        else{
+             while(j<m){
+            ans+=word2[j];
+            j++;
+        }
+        }
+        return ans;
+    }
+};
