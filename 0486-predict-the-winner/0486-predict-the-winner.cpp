78563f51e8060d9deaaace77524bbed4b49117eb
Naman Jain <155368508+engnaman7752@users.noreply.github.com>
2026-08-02 01:55:41 +0530

    Time: 127 ms (9.08%), Space: 10.2 MB (32.41%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool predictTheWinner(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        return solve(0,n-1,dp,nums)>=0;
+    }
+    int solve(int i,int j, vector<vector<int>>&dp,vector<int>&nums){
+        if(i>j)return 0;
+        if(i==j)return nums[i];
+        if(dp[i][j]!=-1)return dp[i][j];
+        return max(nums[i]-solve(i+1,j,dp,nums),nums[j]-solve(i,j-1,dp,nums));
+    }
+};
